3923c7c6c0abc0554920ead1d545766ff391f305
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-11-26 23:34:51 -0500

    Use new decorators

--- src/Controller.ts
+++ src/Controller.ts
@@ -1,9 +1,13 @@
-import { get } from './serve'
+import { get, $, response } from './serve'
+import { ServerResponse } from 'http'
 
 export class Controller {
 
-  @get(/hello/)
-  hello (name: string = 'world') {
+  @get(/hello(?:\/(.*))?/)
+  hello (@$(1) name: string = 'world', @response() response?: ServerResponse) {
+    if (response !== undefined) {
+      response.setHeader('x-timestamp', new Date().toISOString())
+    }
     return `hello ${name}`
   }
 
