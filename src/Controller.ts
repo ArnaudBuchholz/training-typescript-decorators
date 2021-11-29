2402967c97ca501a7693aeff660422478b0f29c3
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-11-29 09:56:45 -0500

    Reformat

--- src/Controller.ts
+++ src/Controller.ts
@@ -2,7 +2,6 @@ import { get, $, response } from './serve'
 import { ServerResponse } from 'http'
 
 export class Controller {
-
   @get(/hello(?:\/(.*))?/)
   hello (@$(1) name: string = 'world', @response() response?: ServerResponse) {
     if (response !== undefined) {
@@ -10,5 +9,4 @@ export class Controller {
     }
     return `hello ${name}`
   }
-
 }
