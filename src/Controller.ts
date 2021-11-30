10c0a89a5e702ce9b23fb079fe235f9f98d69ed4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-11-30 15:43:29 -0500

    Remove response processing

--- src/Controller.ts
+++ src/Controller.ts
@@ -1,12 +1,8 @@
-import { get, $, response } from './serve'
-import { ServerResponse } from 'http'
+import { get, $ } from './serve'
 
 export class Controller {
   @get(/hello(?:\/(.*))?/)
-  hello (@$(1) name: string = 'world', @response() response?: ServerResponse) {
-    if (response !== undefined) {
-      response.setHeader('x-timestamp', new Date().toISOString())
-    }
+  hello (@$(1) name: string = 'world') {
     return `hello ${name}`
   }
 }
