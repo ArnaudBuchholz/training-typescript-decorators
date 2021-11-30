9b3f378cdd1091f87792e255b45f300d3d856010
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-11-30 15:24:24 -0500

    Adds the missing header

--- custom.js
+++ custom.js
@@ -2,7 +2,8 @@ module.exports = (request, response, name = 'World') => {
   const string = `Hello ${name}`
   response.writeHead(200, {
     'content-type': 'text/plain',
-    'content-length': string.length
+    'content-length': string.length,
+    'x-timestamp', new Date().toISOString()
   })
   response.end(string)
 }
